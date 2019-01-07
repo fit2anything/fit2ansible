19d6efce49dbba0e9eaa039ebacedd60b967f86c
ibuler <ibuler@qq.com>
2019-01-07 18:03:24 +0800

    [Update] 增加权限

--- openshift_api/api.py
+++ openshift_api/api.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from ansible_api.permissions import IsSuperUser
 from . import serializers
 from .models import Cluster, Node, Role, DeployExecution, Package
 from .mixin import ClusterResourceAPIMixin
@@ -10,6 +11,7 @@ from .tasks import start_deploy_execution
 class ClusterViewSet(viewsets.ModelViewSet):
     queryset = Cluster.objects.all()
     serializer_class = serializers.ClusterSerializer
+    permission_classes = (IsSuperUser,)
     lookup_field = 'name'
     lookup_url_kwarg = 'name'
 
@@ -18,18 +20,21 @@ class ClusterViewSet(viewsets.ModelViewSet):
 class NodeViewSet(ClusterResourceAPIMixin, viewsets.ModelViewSet):
     queryset = Node.objects.all()
     serializer_class = serializers.NodeSerializer
+    permission_classes = (IsSuperUser,)
     lookup_field = 'name'
     lookup_url_kwarg = 'name'
 
 
 class RoleViewSet(ClusterResourceAPIMixin, viewsets.ModelViewSet):
     queryset = Role.objects.all()
+    permission_classes = (IsSuperUser,)
     serializer_class = serializers.RoleSerializer
 
 
 class DeployExecutionViewSet(ClusterResourceAPIMixin, viewsets.ModelViewSet):
     queryset = DeployExecution.objects.all()
     serializer_class = serializers.DeployExecutionSerializer
+    permission_classes = (IsSuperUser,)
     read_serializer_class = serializers.DeployExecutionSerializer
 
     http_method_names = ['post', 'get', 'head', 'options']
@@ -45,6 +50,7 @@ class DeployExecutionViewSet(ClusterResourceAPIMixin, viewsets.ModelViewSet):
 class PackageViewSet(viewsets.ModelViewSet):
     queryset = Package.objects.all()
     serializer_class = serializers.PackageSerializer
+    permission_classes = (IsSuperUser,)
     http_method_names = ['get', 'head', 'options']
     lookup_field = 'name'
     lookup_url_kwarg = 'name'
