d330f52dc2b63db42f9ab95b0f70e1b529d3980a
ibuler <ibuler@qq.com>
2019-04-17 13:06:58 +0800

    [Update] 兼容版本

--- celery_api/urls/api_urls.py
+++ celery_api/urls/api_urls.py
@@ -7,4 +7,7 @@ app_name = 'celery-api'
 urlpatterns = [
     path('tasks/<uuid:pk>/result/', api.TaskResultApi.as_view(), name='task-result-api'),
     path('tasks/<uuid:pk>/log/', api.TaskLogApi.as_view(), name='task-log-api'),
+    # 兼容之前版本
+    path('celery/task/<uuid:pk>/result/', api.TaskResultApi.as_view(), name='celery-task-result-api'),
+    path('celery/task/<uuid:pk>/log/', api.TaskLogApi.as_view(), name='celery-task-log-api'),
 ]
