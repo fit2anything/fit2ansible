71f2d56eccd2c4df5ba1486e245829f0043348f3
ibuler <ibuler@qq.com>
2019-07-05 11:16:43 +0800

    [Update] 修改callback

--- ansible_api/ansible/callback.py
+++ ansible_api/ansible/callback.py
@@ -24,7 +24,8 @@ class CallbackMixin:
             ok=defaultdict(dict),
             failed=defaultdict(dict),
             unreachable=defaultdict(dict),
-            skippe=defaultdict(dict),
+            skipped=defaultdict(dict),
+            ignored=defaultdict(dict),
         )
         self.results_summary = dict(
             contacted=defaultdict(dict),
@@ -85,8 +86,11 @@ class AdHocResultCallback(CallbackMixin, CallbackModule, CMDCallBackModule):
             dark[host][task_name] = detail
 
     def v2_runner_on_failed(self, result, ignore_errors=False):
-        self.results_summary['success'] = False
-        self.gather_result("failed", result)
+        if ignore_errors:
+            self.gather_result('ignored', result)
+        else:
+            self.results_summary['success'] = False
+            self.gather_result("failed", result)
         super(CallbackModule, self).v2_runner_on_failed(
             result, ignore_errors=ignore_errors
         )
